fc5a2f0c4a865fa41d841954977dd41a8fc985a1
Henry Amponsah <jatturan@outlook.com>
2025-06-18 21:11:46 +0000

    feat(models): add comments model and comments base route

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 
 import userRoutes from './routes/user.routes.js';
 import projectRoutes from './routes/project.routes.js';
+import commentRoutes from './routes/comment.routes.js';
 import errorHandler from './middlewares/error.middleware.js';
 import { NODE_ENV } from './config/index.js';
 import connDB from './config/db.js';
@@ -25,6 +26,7 @@ app.use(express.json());
 // Routes middlewares
 app.use('/api/auth/v1/users', userRoutes);
 app.use('/api/auth/v1/projects', projectRoutes);
+app.use('/api/auth/v1/comments', commentRoutes);
 
 // Custom middleware
 app.use(errorHandler);
---
+++ src/models/comment.model.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const commentSchema = new mongoose.Schema(
+  {
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Project',
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    isEdited: {
+      type: Boolean,
+      default: false,
+    },
+    isFlagged: {
+      type: Boolean,
+      default: false,
+    },
+    parentComment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment',
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+export default Comment;
