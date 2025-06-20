27bafaf73271509e7f47ef617abc0c734a01a76e
Henry Amponsah <jatturan@outlook.com>
2025-06-20 03:14:04 +0000

    feat(controllers): add delete comment endpoint functionality

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import protect from '../middlewares/auth.middleware.js';
 import {
-  createComment,
-  getComment,
   getComments,
+  getComment,
+  createComment,
+  updateComment,
+  deleteComment,
 } from '../controllers/comment.controller.js';
 
 const router = express.Router();
 
 // Routes
 router.route('/').get(protect, getComments);
-
-router.route('/:id').get(protect, getComment).post(protect, createComment);
+router.route('/add').post(protect, createComment);
+router
+  .route('/:id')
+  .get(protect, getComment)
+  .put(protect, updateComment)
+  .delete(protect, deleteComment);
 
 export default router;
